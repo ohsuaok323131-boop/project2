4f69d89823eae9f1b55e9b81471ecf2945348d27
오수아 <ohsuaok323131@gmail.com>
2026-05-02 19:06:54 +0900

    .

---
+++ Person.java
@@ -0,0 +1,22 @@
+
+/**
+ * Person 클래스의 설명을 작성하세요.
+ *
+ * @author (작성자 이름)
+ * @version (버전 번호 또는 작성한 날짜)
+ */
+public class Person {
+    private String name;
+    
+    public Person(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
