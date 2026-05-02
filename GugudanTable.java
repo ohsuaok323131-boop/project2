6231ea0a44de7a1770d3a7bbe4839503e520a59c
오수아 <ohsuaok323131@gmail.com>
2026-05-03 07:40:17 +0900

    Person, Student, GugudanTable, Main 클래스 작성 student-person상속, 구구단 질문 받음(num객체 만들어서 main 클래스의 gu  받음) gugudantable:특정단의 구구단 계산, 출력

--- GugudanTable.java
+++ GugudanTable.java
@@ -5,18 +5,18 @@
  * @author (작성자 이름)
  * @version (버전 번호 또는 작성한 날짜)
  */
-public class MultiplicationTable
+public class GugudanTable
 {
     // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 변경하세요.
-    private int x;
+    private int gu;
 
     /**
      * MultiplicationTable 클래스의 객체 생성자
      */
-    public MultiplicationTable()
+    public GugudanTable(int gu)
     {
         // 인스턴스 변수 초기화
-        x = 0;
+        this.gu=gu;
     }
 
     /**
@@ -25,9 +25,18 @@ public class MultiplicationTable
      * @param  y  메소드의 샘플 파라미터
      * @return    x 더하기 y의 결과값을 반환
      */
-    public int sampleMethod(int y)
+    int result=0;
+    public void calculate()
     {
         // 여기에 코드를 작성하세요.
-        return x + y;
+        for(int i=1;i<=9;i++){
+            result=gu*i;
+        }
+    }
+    
+    public void print(){
+        for(int i=1;i<=9;i++){
+            System.out.println(gu+"x"+i+"="+result);
+        }
     }
 }
---
+++ Student.java
@@ -0,0 +1,37 @@
+
+/**
+ * Student 클래스의 설명을 작성하세요.
+ *
+ * @author (작성자 이름)
+ * @version (버전 번호 또는 작성한 날짜)
+ */
+public class Student extends Person{
+
+    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 변경하세요.
+    private int student;
+
+    /**
+     * Student 클래스의 객체 생성자
+     */
+    public Student(String name, int id){
+        // 인스턴스 변수 초기화
+        super(name);
+        this.student=id;
+    }
+
+    /**
+     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public int getStudent(){
+        return this.student;
+    }
+    
+    public void askGugudan(int gu){
+        GugudanTable num=new GugudanTable(gu);
+        num.calculate();
+        num.print();
+    }
+}
